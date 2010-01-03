b8f9a6a921bf4a780195cabf12982a2ac98e7aef
solostyle <1style@gmail.com>
2010-01-03 13:21:27 -0600

    more framework additions. need to customize

--- config/config.php
+++ config/config.php
@@ -8,3 +8,7 @@ define('DB_NAME', 'iam'); /* placeholder for now */
 define('DB_USER', 'solostyle');
 define('DB_PASSWORD', 'qas??wed');
 define('DB_HOST', 'mysql.solostyle.net');
+
+define('BASE_PATH', 'http://test.solostyle.net/'); /* this means i can put elitetoma in its own folder within test.solostyle.net */
+
+define('PAGINATE_LIMIT'. '5');
--- lib/template.class.php
+++ lib/template.class.php
@@ -1,10 +1,10 @@
 <?php
 class Template {
-
+	
 	protected $variables = array();
 	protected $_controller;
 	protected $_action;
-
+	
 	function __construct($controller,$action) {
 		$this->_controller = $controller;
 		$this->_action = $action;
@@ -16,24 +16,36 @@ class Template {
 		$this->variables[$name] = $value;
 	}
 
-	/** Display Template **/
-
-    function render() {
+	/** Display Template 
+        Displays the template by including the header, the view, and the footer.
+        if doNotRenderHeader is set, only the view is displayed.
+        This is useful for handling AJAX requests.
+    **/
+    function render($doNotRenderHeader = 0) {
+		
+		$html = new HTML;
 		extract($this->variables);
-
+		
+		if ($doNotRenderHeader == 0) {
+			
 			if (file_exists(ROOT . DS . 'app' . DS . 'views' . DS . $this->_controller . DS . 'header.php')) {
 				include (ROOT . DS . 'app' . DS . 'views' . DS . $this->_controller . DS . 'header.php');
 			} else {
 				include (ROOT . DS . 'app' . DS . 'views' . DS . 'header.php');
 			}
+		}
 
-        include (ROOT . DS . 'app' . DS . 'views' . DS . $this->_controller . DS . $this->_action . '.php');		 
-
+		if (file_exists(ROOT . DS . 'app' . DS . 'views' . DS . $this->_controller . DS . $this->_action . '.php')) {
+			include (ROOT . DS . 'app' . DS . 'views' . DS . $this->_controller . DS . $this->_action . '.php');		 
+		}
+			
+		if ($doNotRenderHeader == 0) {
 			if (file_exists(ROOT . DS . 'app' . DS . 'views' . DS . $this->_controller . DS . 'footer.php')) {
 				include (ROOT . DS . 'app' . DS . 'views' . DS . $this->_controller . DS . 'footer.php');
 			} else {
 				include (ROOT . DS . 'app' . DS . 'views' . DS . 'footer.php');
 			}
+		}
     }
 
 }
