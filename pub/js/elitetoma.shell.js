e46f8deea14bb3b3c0941c45364f0880081e062f
solostyle <1style@gmail.com>
2010-02-09 21:51:49 -0600

    privatized a shell.js function

--- pub/js/elitetoma.shell.js
+++ pub/js/elitetoma.shell.js
@@ -1,23 +1,22 @@
 this.Elitetoma.Shell = this.Elitetoma.Shell || function() {
 
+	var handleDomReady = function(obj) {
+		//onDOMReady uses the Custom Event signature, with the object
+		//passed in as the third argument:
+		//type <string>, args <array>, customobject <object>
+		//"DOMReady", [], obj
+		if (console.log) console.log("onDomReady event fired.");
+		
+		// load comments web part
+		Elitetoma.Comments.Load();
+		
+		// load updates web part
+		Elitetoma.Updates.Load();
+	};
+
 	return {
-
-		HandleDomReady: function(obj) {
-			//onDOMReady uses the Custom Event signature, with the object
-			//passed in as the third argument:
-			//type <string>, args <array>, customobject <object>
-			//"DOMReady", [], obj
-			if (console.log) console.log("onDomReady event fired.");
-			
-			// load comments web part
-			Elitetoma.Comments.Load();
-
-			// load updates web part
-			Elitetoma.Updates.Load();
-		},
-
 		LoadWebParts: function() {
-			Yevent.onDOMReady(this.HandleDomReady);
+			Yevent.onDOMReady(handleDomReady);
 		}
 	};
 
