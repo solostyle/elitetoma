7b9993e60ea8aa29cc4b3971d10d054061cbf9f5
solostyle <1style@gmail.com>
2010-02-09 22:05:25 -0600

    added new webpart class. each web part in the shell is of class webpart. moved some redundant css styles to base.css

--- app/views/shells/index.php
+++ app/views/shells/index.php
@@ -1,7 +1,7 @@
-<div id="commentsWP"> 
+<div id="commentsWP" class="webpart"> 
      <!-- <div id="comments"></div> -->
 </div>
 
-<div id="updatesWP">
+<div id="updatesWP" class="webpart">
      <!-- <div id="updates"></div> -->
 </div>
