3e91beb1f371fa0242aafd7a7acf8aaa8cbfb3a6
solostyle <1style@gmail.com>
2010-02-09 21:57:40 -0600

    removed inner webpart divs from shells/index.php view

--- app/views/shells/index.php
+++ app/views/shells/index.php
@@ -1,7 +1,7 @@
 <div id="commentsWP"> 
-     <div id="comments"></div>
+     <!-- <div id="comments"></div> -->
 </div>
 
 <div id="updatesWP">
-     <div id="updates"></div>
+     <!-- <div id="updates"></div> -->
 </div>
